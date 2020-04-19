8f5451938cf8d045caf32982fd6df694782b2038
ShoujiKanenobu <shoujikanenobu@gmail.com>
2020-04-19 14:50:43 -0700

    more bugs, oh my

---
+++ Assets/_Scripts/DeletePrefs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DeletePrefs : EditorWindow
+{
+    [MenuItem("Window/Prefs")]
+    public static void ShowWindow()
+    {
+        GetWindow<DeletePrefs>("DeletePrefs");
+        
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Player Prefs Reset", EditorStyles.boldLabel);
+        
+        if(GUILayout.Button("Reset High Score"))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
